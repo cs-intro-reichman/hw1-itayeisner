c4478eadf7524429ccd103cbdfe401d42ef4c449
Itay Eisner <itay.eisner@post.runi.ac.il>
2024-11-06 17:04:09 +0200

    Update Ascend.java

--- Ascend.java
+++ Ascend.java
@@ -3,5 +3,16 @@
 public class Ascend {
 	public static void main(String[] args) {
 		// Replace this comment with your code
+		int lim = Integer.parseInt(args [0]);
+		int a = (int)(Math.random()*lim);
+		int b = (int)(Math.random()*lim);
+		int c = (int)(Math.random()*lim);
+
+		int max = Math.max(Math.max(a,b),c);
+		int min = Math.min(Math.min(a,b),c);
+		int center = a+b+c-max-min;
+
+		System.out.println(a+" "+b+" "+c);
+		System.out.println(min+" "+center+" "+max);
 	}
 }
