491376e1444ef9c5f7dbd38b4e21d0537484d91d
Itay Eisner <itay.eisner@post.runi.ac.il>
2024-11-06 15:07:15 +0200

    Update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -7,12 +7,12 @@ public class Bill3 {
 	   String name2 = args[1];
 	   String name3 = args[2];
 		int samount = Integer.parseInt(args[3]);
-		
+
 		// calc the payment per person + ciel up
 		double each = (samount)/3.0;
 		   double ans = Math.ceil( each );
 		
-		 System.out.print("Dear " + name1 + ", " + name2 + ", " + name3  + ": Pay " + ans + " Shekels each");
+		 System.out.print("Dear " + name3 + ", " + name2 + ", and " + name1  + ": Pay " + ans + " Shekels each.");
 	
 		}
 	}
