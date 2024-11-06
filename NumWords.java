ba5edf1d8713bc244b338e218c1ca634b6ab09ee
Itay Eisner <itay.eisner@post.runi.ac.il>
2024-11-06 16:28:25 +0200

    Update NumWords.java

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,12 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int innum = Integer.parseInt(args [0]);
+		int ones = innum%10;
+		innum = innum/10;
+		int tens = innum%10;
+		innum = innum/10;
+		int hundreds = innum;
+		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 	}
 }
