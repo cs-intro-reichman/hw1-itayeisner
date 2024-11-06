f102e1ec50a2e6eb8ee5f98f4c179c04f69422f4
Itay Eisner <itay.eisner@post.runi.ac.il>
2024-11-06 15:53:48 +0200

    Update FVCalc.java

--- FVCalc.java
+++ FVCalc.java
@@ -2,5 +2,11 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
+		int currentValue = Integer.parseInt( args [0]);
+		double rate = Double.parseDouble( args [1]);
+		int years = Integer.parseInt( args [2]);
+		double fv = currentValue*(Math.pow((double)(1+rate/100),(double)years));
+		// 
+		System.out.println("After " + years + " years, $" + currentValue + " saved at " + rate + "% will yield $" + (int) fv);
 	}
 }
